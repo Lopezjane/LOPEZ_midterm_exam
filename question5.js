6457f73e3fe1f918f99c6684eae1ccb908cfd59e
unknown <YY531@ccs.local>
2025-03-15 14:42:20 +0800

    db connected

--- question5.js
+++ question5.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 //database connection
 const sequelize = new Sequelize('LOPEZ_midterm_exam', 'root', '', {
